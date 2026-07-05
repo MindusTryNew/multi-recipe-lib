b14817090476a379adc9151afa43c8fc6dbb23fd
MindusTryNew <zugtrain77@gmail.com>
2026-07-05 14:56:49 +0200

    Implement multi-crafter block and recipes
    Added multi-crafter block and example recipes.

--- src/multirecipe/MultiRecipeLib.java
+++ src/multirecipe/MultiRecipeLib.java
@@ -1,30 +1,33 @@
 package multirecipe;
 
+import arc.util.Log;
 import mindustry.mod.Mod;
-import mindustry.content.Blocks;
 
-/**
- * Main entry point for Multi Recipe Library
- * This mod extends Mindustry and adds multi-recipe crafting support.
- */
 public class MultiRecipeLib extends Mod {
 
-    public static MultiRecipeLib instance;
-
-    public MultiRecipeLib(){
-        instance = this;
-    }
+    public static MultiCrafterBlock multiCrafter;
 
     @Override
     public void loadContent(){
-        // This is called when Mindustry loads content
 
-        // Placeholder log (you'll see this in console later)
+        Log.info("Loading MultiRecipeLib...");
+
+        // ---------------- BLOCK REGISTRATION ----------------
+        multiCrafter = new MultiCrafterBlock("multi-crafter");
+
+        // Beispiel-Rezepte (nur Test!)
+        Recipe r1 = new Recipe(60f)
+                .itemIn(mindustry.content.Items.copper, 2)
+                .itemIn(mindustry.content.Items.lead, 1)
+                .itemOut(mindustry.content.Items.graphite, 1);
+
+        Recipe r2 = new Recipe(90f)
+                .itemIn(mindustry.content.Items.sand, 2)
+                .itemOut(mindustry.content.Items.metaglass, 1);
+
+        multiCrafter.recipes.add(r1);
+        multiCrafter.recipes.add(r2);
+
         Log.info("MultiRecipeLib loaded successfully.");
-
-        // Here we will later register:
-        // - MultiCrafter blocks
-        // - Recipe system
-        // - Parsers
     }
-}
+} 
